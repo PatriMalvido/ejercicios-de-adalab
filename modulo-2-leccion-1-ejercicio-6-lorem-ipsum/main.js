43a0aa70f70dd72ceaa7c0cbd83c9be4f64dc8f4
Patri <patri@malvidopazo.es>
2021-11-29 19:06:21 +0100

    add new exercises

---
+++ modulo-2-leccion-1-ejercicio-6-lorem-ipsum/main.js
@@ -0,0 +1,7 @@
+"use sctrict";
+
+const listElement = document.querySelector (".list");
+
+const content= '<h1>Lorem ipsum</h1><img src= "http://via.placeholder.com/350x150"></img><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit</p>';
+
+listElement.innerHTML= content;
