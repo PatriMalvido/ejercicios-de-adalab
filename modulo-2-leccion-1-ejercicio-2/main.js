c6ef1413240504c53251686db4252f1d4c06a907
Patri <patri@malvidopazo.es>
2021-11-29 17:21:57 +0100

    add new exercises

---
+++ modulo-2-leccion-1-ejercicio-2/main.js
@@ -0,0 +1,8 @@
+"use strict";
+/*
+const houseAdress = "c/As Teixugueiras, 29";
+const houseAdress = "c/Florida,32";
+
+así da error, porque a la misma constante le asigno dos valores. La manera correcta seria la siguiente:*/
+const houseAdressPatri ="c/As Teixugueiras";
+const houseAdressPepa = "c/Florida, 32";
