5262fafa0f54d34c9db845ba9f2c41ff7cb73c80
Patri <patri@malvidopazo.es>
2021-12-16 09:23:15 +0100

    add exercices

--- modulo-2-leccion-9-ejercicio-3-the-numbers/main.js
+++ modulo-2-leccion-9-ejercicio-3-the-numbers/main.js
@@ -1,6 +1,8 @@
 "use strict";
 
 //Ejercicio 3.1
+//Crear un nuevo array que contiene solo los números pares que hay en ////////////lostNumbers. Para conseguirlo vamos a crear un nuevo array vacío y recorrer ////el array lostNumbers para al encontrar un número par, meterlo en el nuevo //////array.
+
 const lostNumbers = [4, 8, 15, 16, 23, 42];
 
 function bestLostNumber(numbers) {
@@ -19,6 +21,8 @@ console.log(bestLostNumber(lostNumbers));
 // en la funcion bestLostNumber paso como parametro numbers, para que sea cual sea el valor de la constante declarada fuera de la funcion no me afecte y la funcion se pueda reutilizar.
 
 //Ejercicio 3.2
+//Crear un nuevo array que contiene solo los números múltiplos de 3 que hay en ///lostNumbers, de una forma similar al anterior.
+
 function bestLostNumberTwo(numbers) {
   const myArray = [];
 
@@ -31,6 +35,8 @@ function bestLostNumberTwo(numbers) {
 }
 console.log(bestLostNumberTwo(lostNumbers));
 
+
 //Ejercicio 3.3
+//Devolver una concatenación de los 2 arrays anteriores, es decir, que tendrá ////primero los números pares y luego los múltiplos de 3.
 
 console.log(bestLostNumber(lostNumbers).concat(bestLostNumberTwo(lostNumbers)));
